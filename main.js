1017106d17bf0e6f2f5e23c6d7f75d68b68fa463
echchaari <echchaari.brahim@gmail.com>
2018-10-25 10:14:57 -0500

    One Cat Clicker

---
+++ main.js
@@ -0,0 +1,13 @@
+//console.log("hello");
+document.querySelector("#cat").addEventListener("click", onClick);
+
+let counter = 0;
+
+const counterEle = document.querySelector("#counter");
+
+counterEle.textContent = counter;
+
+function onClick() {
+    counter++;
+    counterEle.textContent = counter;
+}
